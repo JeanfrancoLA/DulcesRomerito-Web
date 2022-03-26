51decfc292462fabc9d82a8c1869be5ab620ca8e
JeanfrancoLA <genkoz98@gmail.com>
2022-03-26 16:30:48 -0500

    :React: Instalacion de Router-Dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import './App.css';
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  NavLink
+} from 'react-router-dom';
+
 
 function App() {
   return (
-    <>
-      <h1>Inicio del Proyecto</h1>
-    </>
+    <Router>
+        <NavLink to="/inicio">
+          Inicio
+        </NavLink>
+        <br/>
+        <NavLink to="/nosotros">
+          Nosotros
+        </NavLink>
+        <br/>
+        <NavLink to="/startpage">
+          StartPage
+        </NavLink>
+        <Routes>
+          <Route path="/sobre-nosotros" element={<div>Nosotros</div>} />
+          <Route path="/inicio" element={<div>inicio</div>} />
+          <Route path="/startpage" element={<div>startpage</div>} />
+          <Route path="/" element={<div></div>} />
+        </Routes>
       
+    </Router>
   );
 }
 
